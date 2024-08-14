3d5b69f13d83ec64b9fa38fabaaaa6ad9cf10d8b
davcarl <116964287+davcarl@users.noreply.github.com>
2024-08-14 16:51:20 +0300

    cor

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,34 @@
-#!/usr/bin/python3
-"""Module for task 0"""
 
+#!/usr/bin/python3
+"""Queries the Reddit API and
+returns the number of subscribers
+(not active users, total subscribers)
+for a given subreddit.
+If an invalid subreddit is given,
+the function should return 0.
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers
-    to the subreddit"""
+    """Returns the total number of subscribers
+    for a given subreddit.
+    """
+    # Set the Default URL strings
+    base_url = 'https://www.reddit.com'
+    api_uri = '{base}/r/{subreddit}/about.json'.format(base=base_url,
+                                                       subreddit=subreddit)
 
-    # Construct the URL for subreddit information
-    sub_info_url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    # Set an User-Agent
+    user_agent = {'User-Agent': 'Python/requests'}
 
-    # Set custom User-Agent header
-    headers = {"User-Agent": "Ubuntu 20.04"}
+    # Get the Response of the Reddit API
+    res = requests.get(api_uri, headers=user_agent,
+                       allow_redirects=False)
 
-    # Make the GET request to the subreddit information URL
-    response = requests.get(
-        sub_info_url, headers=headers, allow_redirects=False)
+    # Checks if the subreddit is invalid
+    if res.status_code in [302, 404]:
+        return 0
 
-    if response.status_code == 200:
-        return response.json()['data']['subscribers']
-    return 0
+    # Returns the total subscribers of the subreddit
+    return res.json().get('data').get('subscribers')
