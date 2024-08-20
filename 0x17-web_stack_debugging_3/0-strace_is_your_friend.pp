5c8cd6956e2b07295f536e62beccbcc66b917f83
davcarl <116964287+davcarl@users.noreply.github.com>
2024-08-20 13:10:09 +0300

    correction

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # puppet script that fixes the 500 error 
 #
-# exec { 'fix-wordpress':
+exec { 'fix-wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/'
 }
